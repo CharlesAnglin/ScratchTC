362149b77fc3f35405ec107a11d61e37229eac7b
charles anglin <charles.anglin@digital.hmrc.gov.uk>
2017-11-26 11:42:22 +0000

    renaming folders

---
+++ 5exportTest/pingRequest3.js
@@ -0,0 +1,7 @@
+Blockly.PHP['pingrequest3'] = function(block) {
+  var text_filename = block.getFieldValue('fileName');
+  var statements_requestparams = Blockly.PHP.statementToCode(block, 'requestParams');
+  // TODO: Assemble PHP into code variable.
+  var code = '...;\n';
+  return code;
+};
---
+++ 5exportTest/saveFile2.php
@@ -0,0 +1,16 @@
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fileName = $_GET["fileName"];
+
+    $body = file_get_contents('php://input');
+    $myFile = fopen("myEndpoints/".$fileName, "w");
+    fwrite($myFile, $body);
+
+    http_response_code(200);
+    echo("created " . $fileName);
+  }
+  else {
+    http_response_code(503);
+    echo('Use a post request with the "fileName" as a url parameter and the php code as the post body');
+  }
+?>
