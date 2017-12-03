376f3e55153879f1732678b954188449a7c70917
charles anglin <charles.anglin@digital.hmrc.gov.uk>
2017-12-03 15:47:37 +0000

    rearrangedFiles to be clearer and easier to host

---
+++ 11websiteHosting/api/abababa.php
@@ -0,0 +1,15 @@
+<?php
+$x;
+$ad;
+$boooop;
+$zzzzzz;
+
+function unnamed($ad, $boooop, $zzzzzz) {
+  global $x;
+}
+
+
+echo(unnamed($_GET["ad"],$_GET["boooop"],$_GET["zzzzzz"]));
+http_response_code(200);
+
+?>
---
+++ 11websiteHosting/api/myRequest.php
@@ -0,0 +1,12 @@
+<?php
+$x;
+$y;
+
+function unnamed($x, $y) {
+}
+
+
+echo(unnamed($_GET["x"],$_GET["y"]));
+http_response_code(200);
+
+?>
